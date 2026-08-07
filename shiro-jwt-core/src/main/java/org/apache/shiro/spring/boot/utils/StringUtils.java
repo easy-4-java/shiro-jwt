023bda61dccafda2526775d689ce809ca8559918
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 01:31:38 +0800

    refactor: split shiro jwt into core and spring modules

---
+++ shiro-jwt-core/src/main/java/org/apache/shiro/spring/boot/utils/StringUtils.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.shiro.spring.boot.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class StringUtils {
+
+	/**
+	 * Any number of these characters are considered delimiters between multiple
+	 * context config paths in a single String value.
+	 */
+	public static String CONFIG_LOCATION_DELIMITERS = ",; \t\n";
+	
+	public static String[] tokenizeToStringArray(String str) {
+		if (org.apache.commons.lang3.StringUtils.isEmpty(str)) {
+			return new String[0];
+		}
+		StringTokenizer tokenizer = new StringTokenizer(str, CONFIG_LOCATION_DELIMITERS);
+		List<String> tokens = new ArrayList<>();
+		while (tokenizer.hasMoreTokens()) {
+			tokens.add(tokenizer.nextToken());
+		}
+		return tokens.toArray(new String[0]);
+	}
+	
+}
---
+++ shiro-jwt-spring/src/main/java/org/apache/shiro/spring/boot/jwt/JwtPrincipalRepository.java
@@ -0,0 +1,50 @@
+package org.apache.shiro.spring.boot.jwt;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.biz.authz.principal.ShiroPrincipalRepositoryImpl;
+import org.apache.shiro.spring.boot.jwt.token.JwtAuthorizationToken;
+
+import io.github.easy4j.jwt.JwtPayload;
+
+public class JwtPrincipalRepository extends ShiroPrincipalRepositoryImpl {
+
+	private final JwtPayloadRepository jwtPayloadRepository;
+	private boolean checkExpiry = false;
+
+	public JwtPrincipalRepository(JwtPayloadRepository jwtPayloadRepository) {
+		this.jwtPayloadRepository = jwtPayloadRepository;
+	}
+
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		JwtAuthorizationToken jwtToken = (JwtAuthorizationToken) token;
+		JwtPayload payload = getJwtPayloadRepository().getPayload(jwtToken, isCheckExpiry());
+		JwtPayloadPrincipal principal = new JwtPayloadPrincipal(payload);
+		principal.setPerms(payload.getPerms());
+		principal.setRole(payload.getRkey());
+		principal.setRoles(payload.getRoles());
+		principal.setRoleid(payload.getRid());
+		principal.setUserid(payload.getSubject());
+		principal.setUserkey(payload.getUkey());
+		principal.setUsercode(payload.getUcode());
+		principal.setInitial(payload.isInitial());
+		principal.setProfile(payload.getProfile());
+		return new SimpleAuthenticationInfo(principal, jwtToken.getCredentials(), "JWT");
+	}
+
+	public JwtPayloadRepository getJwtPayloadRepository() {
+		return jwtPayloadRepository;
+	}
+
+	public boolean isCheckExpiry() {
+		return checkExpiry;
+	}
+
+	public void setCheckExpiry(boolean checkExpiry) {
+		this.checkExpiry = checkExpiry;
+	}
+
+}
